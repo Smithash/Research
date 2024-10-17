227f32cb5ce121f41a2b5c612b0731f43e04964e
Ashlea <smithashlea@gmail.com>
2024-10-17 17:04:06 +0200

    oct dataset

--- deepset-simclr/src/data/OctDukeDataset.py
+++ deepset-simclr/src/data/OctDukeDataset.py
@@ -1,91 +1,58 @@
 import os
 import random
+from typing import List, Tuple
 import numpy as np
-import scipy.io
+from PIL import Image
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 
-class AdaptedOCTDataset(Dataset):
-    def __init__(self, scan_paths, image_paths, transform=None, config=None):
-        self.scan_paths = scan_paths
-        self.image_paths = image_paths
+class OCTDataset(Dataset):
+    def __init__(self, root_dir: str, scan_folders: List[str], transform=None):
+        self.root_dir = root_dir
+        self.scan_folders = scan_folders
         self.transform = transform
-        self.config = config
-        self.valid_data = []
+        self.image_paths = self._get_image_paths()
 
-        self._process_all_files()
-
-    def _process_all_files(self):
-        for file_path in self.image_paths:
-            mat = scipy.io.loadmat(file_path)
-
-            images = mat['images']
-            x, y, nimages = images.shape
-            step = 4
-            ini = int(y / step)
-            fin = int(ini * (step - 1))
-
-            for i in range(nimages):
-                curr_im = images[:, ini:fin, i]
-                image = curr_im.astype(np.float32)
-                self.valid_data.append((image, file_path))
+    def _get_image_paths(self) -> List[Tuple[str, int]]:
+        image_paths = []
+        for folder in self.scan_folders:
+            folder_path = os.path.join(self.root_dir, folder)
+            for file in os.listdir(folder_path):
+                if file.endswith('.jpg'):
+                    try:
+                        # Extract the index from the filename
+                        index = int(file.split('-')[-1].split('.')[0])
+                        image_paths.append((os.path.join(folder_path, file), index))
+                    except ValueError:
+                        print(f"Skipping file {file} as it doesn't match the expected format.")
+        return sorted(image_paths, key=lambda x: (x[0], x[1]))  # Sort by folder and then by index
 
     def __len__(self):
-        return len(self.valid_data)
+        return len(self.image_paths)
 
     def __getitem__(self, idx):
-        image, file_path = self.valid_data[idx]
+        img_path, _ = self.image_paths[idx]
+        image = Image.open(img_path).convert('RGB')
 
         if self.transform:
-            image1 = self.transform(image)
-            image2 = self.transform(image)
+            aug1 = self.transform(image)
+            aug2 = self.transform(image)
+        else:
+            aug1 = transforms.ToTensor()(image)
+            aug2 = transforms.ToTensor()(image)
 
-        return image1, image2, file_path
+        return aug1, aug2, img_path
 
 def get_oct_dataset(config, train_transform, val_transform):
-    train_data_dir = config.data.dataset_root
-    val_data_dir = config.data.dataset_root  # Assuming validation data is in the same directory
-
-    train_scans = [os.path.join(train_data_dir, f) for f in os.listdir(train_data_dir) if f.endswith('.mat')]
-    val_scans = [os.path.join(val_data_dir, f) for f in os.listdir(val_data_dir) if f.endswith('.mat')]
-
-    # Shuffle and split the scans for train and validation
-    random.shuffle(train_scans)
-    split_index = int(0.8 * len(train_scans))  # 80% for training, 20% for validation
-    train_scans, val_scans = train_scans[:split_index], train_scans[split_index:]
-
-    # train_dataset = AdaptedOCTDataset(
-    #     train_scans, train_scans, transform=train_transform, config=config
-    # )
-
-    # val_dataset = AdaptedOCTDataset(
-    #     val_scans, val_scans, transform=val_transform, config=config
-    # )
+    dataset_root = config.data.dataset_root
+    all_scans = [f for f in os.listdir(dataset_root) if os.path.isdir(os.path.join(dataset_root, f))]
     
-    if config.data.dataset_type == 'per_scan':
-        logging.info('Initialising per-scan dataset')
-        train_dataset = SimCLRNLSTPerScanAugDataset(
-            dummy_train_paths, dummy_train_scans, train_transform, config
-        )
+    # Shuffle and split the scans for train and validation
+    random.shuffle(all_scans)
+    split_index = int(0.8 * len(all_scans))  # 80% for training, 20% for validation
+    train_scans, val_scans = all_scans[:split_index], all_scans[split_index:]
 
-        val_dataset = SimCLRNLSTPerScanAugDataset(
-            dummy_val_paths, dummy_val_scans, val_transform, config
-        )
-    elif config.data.dataset_type == 'deepset':
-        logging.info('Initialising Deep Set dataset')
-        train_dataset = SimCLRNLSTDeepSetDataset(
-            dummy_train_paths, dummy_train_scans, config
-        )
-
-        val_dataset = SimCLRNLSTDeepSetDataset(
-            dummy_val_paths, dummy_val_scans, config
-        )
-    else:
-        raise Exception(f'Dataset type not supported: {config.data.dataset_type}')
-
-    logging.info(
-        'Initialised dummy dataset: Train=%s, Val=%s',
-        len(train_dataset), len(val_dataset)
-    )
+    train_dataset = OCTDataset(dataset_root, train_scans, transform=train_transform)
+    val_dataset = OCTDataset(dataset_root, val_scans, transform=val_transform)
 
     return train_dataset, val_dataset
