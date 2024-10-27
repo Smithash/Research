48717f577108056922984017a46b7bd7a7043aee
Ashlea <smithashlea@gmail.com>
2024-10-27 20:39:31 +0200

    view formatting

--- deepset-simclr/src/trainer.py
+++ deepset-simclr/src/trainer.py
@@ -37,8 +37,10 @@ class Trainer:
 
             self.optimiser.param_groups[0]['lr'] = self.lr_schedule[global_iteration]
 
-            view1 = batch[0].to(self.config.optim.device)
-            view2 = batch[1].to(self.config.optim.device)
+            # view1 = batch[0].to(self.config.optim.device)
+            # view2 = batch[1].to(self.config.optim.device)
+            
+            
             # print(f"Type of batch: {type(batch)}")
             # print(f"Length of batch: {len(batch)}")
             # print(f"Type of batch[0]: {type(batch[0])}")
@@ -48,16 +50,16 @@ class Trainer:
             #     if isinstance(batch[0][0], torch.Tensor):
             #         print(f"Shape of batch[0][0]: {batch[0][0].shape}")
 
-            # view1 = torch.stack([item.to(self.config.optim.device) for item in batch[0]])
-            # view2 = torch.stack([item.to(self.config.optim.device) for item in batch[1]])
+            view1 = torch.stack([item.to(self.config.optim.device) for item in batch[0]])
+            view2 = torch.stack([item.to(self.config.optim.device) for item in batch[1]])
 
             # # More debug prints
             # # print(f"Shape of view1 after stacking: {view1.shape}")
             # # print(f"Shape of view2 after stacking: {view2.shape}")
 
             # # Reshape if necessary
-            # view1 = view1.view(-1, 3, 512, 512)
-            # view2 = view2.view(-1, 3, 512,512)
+            view1 = view1.view(-1, 3, 512, 512)
+            view2 = view2.view(-1, 3, 512,512)
 
             # print(f"Final shape of view1: {view1.shape}")
             # print(f"Final shape of view2: {view2.shape}")
@@ -86,15 +88,15 @@ class Trainer:
         val_loss = 0.0
         with torch.no_grad():
             for batch in val_loader:
-                view1 = batch[0].to(self.config.optim.device)
-                view2 = batch[1].to(self.config.optim.device)
-                # view1 = torch.stack([item.to(self.config.optim.device) for item in batch[0]])
-                # view2 = torch.stack([item.to(self.config.optim.device) for item in batch[1]])
+                # view1 = batch[0].to(self.config.optim.device)
+                # view2 = batch[1].to(self.config.optim.device)
+                view1 = torch.stack([item.to(self.config.optim.device) for item in batch[0]])
+                view2 = torch.stack([item.to(self.config.optim.device) for item in batch[1]])
 
 
-                # # Reshape if necessary
-                # view1 = view1.view(-1, 3, 512,512)
-                # view2 = view2.view(-1, 3, 512, 512)
+                # Reshape if necessary
+                view1 = view1.view(-1, 3, 512,512)
+                view2 = view2.view(-1, 3, 512, 512)
                 
                 z1 = self.model(view1)
                 z2 = self.model(view2)
