49c3939a47c7b2f4d895ac388c56e4336adb541f
Ashlea <smithashlea@gmail.com>
2024-10-27 13:20:16 +0200

    image size

--- deepset-simclr/src/data/augmentations.py
+++ deepset-simclr/src/data/augmentations.py
@@ -168,7 +168,7 @@ def get_transform(transform_name, config: Config):
         #     min_scale = 0.67,
         #     normalize={"mean": MEAN, "std": STD}
         # )
-        tfm = SimCLRTransformNoRandomResizedCrop()
+        tfm = SimCLRTransformNoRandomResizedCrop(config.data.image_size)
     elif transform_name == 'deepset':
         tfm = DeepSetTransform(config)
     else:
